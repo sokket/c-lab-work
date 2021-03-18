e3f597266d395f5b999bd7277044655afcf52326
sokket <kn@typex.one>
2021-03-18 23:27:03 +0300

    Performance reduced because of teacher mindset

--- main.c
+++ main.c
@@ -2,49 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void* malloc_wr(size_t size) {
-    void* result = malloc(size);
-    if (result)
-        return result;
-    else {
-        perror("malloc");
-        exit(1);
-    }
-}
-
-typedef struct node {
-    char *str;
-    struct node *next;
-} node;
-
-node *add(node *list, char *str) {
-    if (!list) {
-        list = malloc_wr(sizeof(node));
-        list->next = 0;
-        list->str = str;
-    } else {
-        node *iter = list;
-        for (; iter->next; iter = iter->next) {}
-        iter->next = malloc_wr(sizeof(node));
-        iter = iter->next;
-        iter->next = 0;
-        iter->str = str;
-    }
-    return list;
-}
-
-void free_list(node *head) {
-    node *tmp;
-    while (head != NULL) {
-        tmp = head;
-        head = head->next;
-        free(tmp->str);
-        free(tmp);
-    }
-}
-
 char *get_line_impl() {
-    char *line = malloc_wr(10 * sizeof(char));
+    char *line = malloc(10 * sizeof(char));
     int last_index = 0;
     for (;;) {
         scanf("%1[^\r]", &line[last_index]);
@@ -62,71 +21,64 @@ char *get_line_impl() {
     return line;
 }
 
-int contains(node *list, char *word) {
-    for (node *iter = list; iter; iter = iter->next) {
-        if (!strcmp(word, iter->str)) {
-            return 1;
-        }
-    }
-    return 0;
-}
-
 // Words from str1 that str2 contains
 char *process(char *str1, char *str2) {
-    node *words_from_str2 = 0;
-
-    int start = 0;
-    size_t str2_len = strlen(str2);
-    for (int i = 0; i <= str2_len; i++) {
-        if (start != -1 && (i == str2_len || (str2[i] == ' ' || str2[i] == '\t'))) {
-            int end = i - 1;
-            int len = end - start + 1;
-            char *word = malloc_wr((len + 1) * sizeof(char));
-            strncpy(word, &str2[start], len);
-            word[len] = 0;
-            words_from_str2 = add(words_from_str2, word);
-            start = -1;
-        }
-        if (i != str2_len && str2[i] != ' ' && str2[i] != '\t' && start == -1) {
-            start = i;
-        }
-    }
-
     char *result = 0;
     int result_len = 0;
 
     size_t str1_len = strlen(str1);
-    for (int i = 0; i <= str1_len; i++) {
-        if (start != -1 && (i == str1_len || (str1[i] == ' ' || str1[i] == '\t'))) {
-            int end = i - 1;
+    size_t str2_len = strlen(str2);
+
+
+    int start2 = 0;
+    for (int j = 0; j <= str1_len; j++) {
+        if (start2 != -1 && (j == str1_len || (str1[j] == ' ' || str1[j] == '\t'))) {
+            int end2 = j - 1;
             // Hello 4 - 0 + 1 = 5
-            int word_len = end - start + 1;
+            int word_len = end2 - start2 + 1;
             char tmp[word_len + 1];
-            strncpy(tmp, &str1[start], word_len);
+            strncpy(tmp, &str1[start2], word_len);
             tmp[word_len] = 0;
-            if (contains(words_from_str2, tmp)) {
-                if (!result) {
-                    // terminator + space => 2
-                    result = malloc_wr((word_len + 2) * sizeof(char));
-                    result_len = word_len + 1;
-                    strncpy(result, tmp, word_len);
-                    result[word_len] = 0;
-                } else {
-                    result_len += word_len + 1;
-                    result = realloc(result, result_len + 1);
-                    strncat(result, tmp, word_len);
+
+            int start = 0;
+            for (int i = 0; i <= str2_len; i++) {
+                if (start != -1 && (i == str2_len || (str2[i] == ' ' || str2[i] == '\t'))) {
+                    int end = i - 1;
+                    int len = end - start + 1;
+                    char word[len + 1];
+                    strncpy(word, &str2[start], len);
+                    word[len] = 0;
+                    //words_from_str2 = add(words_from_str2, word);
+
+                    if (!strcmp(word, tmp)) {
+                        if (!result) {
+                            // terminator + space => 2
+                            result = malloc((word_len + 2) * sizeof(char));
+                            result_len = word_len + 1;
+                            strncpy(result, tmp, word_len);
+                            result[word_len] = 0;
+                        } else {
+                            result_len += word_len + 1;
+                            result = realloc(result, result_len + 1);
+                            strncat(result, tmp, word_len);
+                        }
+                        strcat(result, " ");
+                        break;
+                    }
+
+                    // END
+                    start = -1;
                 }
-                strcat(result, " ");
+                if (i != str2_len && str2[i] != ' ' && str2[i] != '\t' && start == -1) {
+                    start = i;
+                }
             }
-            start = -1;
+            start2 = -1;
         }
-        if (i != str1_len && str1[i] != ' ' && str1[i] != '\t' && start == -1) {
-            start = i;
+        if (j != str1_len && str1[j] != ' ' && str1[j] != '\t' && start2 == -1) {
+            start2 = j;
         }
     }
-
-    free_list(words_from_str2);
-
     return result;
 }
 
